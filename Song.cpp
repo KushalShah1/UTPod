30037becace91dd18cea1ecdc7200a35212a8383
KushalShah1 <42986570+KushalShah1@users.noreply.github.com>
2019-10-23 18:17:42 -0500

    Initial Song Finished

--- Song.cpp
+++ Song.cpp
@@ -3,3 +3,71 @@
 //
 
 #include "Song.h"
+
+
+Song::Song(){
+    size= 0;
+}
+Song::Song(string _title, string _artist, int _size) {
+    title=_title;
+    artist=_artist;
+    size=_size;
+}
+string Song::getArtist() const {
+    return artist;
+}
+string Song::getTitle() const {
+    return title;
+}
+int Song::getSize() const {
+    return size;
+}
+
+void Song::setTitle(string _title) {
+    title=_title;
+}
+void Song::setArtist(string _artist) {
+    artist=_artist;
+}
+void Song::setSize(int _size) {
+    size=_size;
+}
+bool Song::operator<(Song const &song) {
+    if(artist.compare(song.getArtist())<0){
+        return true;
+    }
+    else if( artist==song.getArtist()){
+        if(title.compare(song.getTitle())<0){
+            return true;
+        }
+        else if (title==song.getTitle()) {
+            return size < song.getSize();
+        }
+        else
+            return false;
+    }
+    else
+        return false;
+}
+bool Song::operator>(Song const &song) {
+    if(artist.compare(song.getArtist())>0){
+        return true;
+    }
+    else if( artist==getArtist()){
+        if(title.compare(song.getTitle())>0){
+            return true;
+        }
+        else if (title==song.getTitle()) {
+            return size > song.getSize();
+        }
+        else
+            return false;
+    }
+    else
+        return false;
+}
+bool Song::operator==(Song const &song) {
+    return (artist == song.getArtist()) && (title == song.getTitle()) && (size == song.getSize());
+}
+
+
--- Song.h
+++ Song.h
@@ -5,8 +5,26 @@
 #ifndef UTPOD_SONG_H
 #define UTPOD_SONG_H
 
-
+#include <string>
+#include <cstring>
+using namespace std;
 class Song {
+    private:
+        string title;
+        string artist;
+        int size;
+    public:
+        Song();
+        Song(string _title, string _artist, int _size);
+        string getTitle() const;
+        string getArtist() const;
+        int getSize() const;
+        void setTitle(string _title);
+        void setArtist(string _artist);
+        void setSize(int _size);
+        bool operator >(Song const &song);
+        bool operator <(Song const &song);
+        bool operator ==(Song const &song);
 
 };
 
